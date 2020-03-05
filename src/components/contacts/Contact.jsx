453642e6060a054f3aef402d7a45c9a9b9191880
Paulo Rogério Sales Santos <paulosales@gmail.com>
2020-03-04 23:15:59 -0300

    contact section created

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { injectIntl } from 'react-intl';
+import PropTypes from 'prop-types';
+
 import {
   ContactContainer,
   SocialNetworks,
@@ -9,50 +12,86 @@ import {
   FormRow,
   InputForm,
   MessageText,
+  SocialNetwork,
+  SendMessageButton,
+  FormFooter,
 } from './Styles';
-import { FormattedMessage } from 'react-intl';
+import ButtonIcon from '../buttons/ButtonIcon';
 
-export default function PageFooter() {
+const PageFooter = function(props) {
+  const { intl } = props;
   return (
     <ContactContainer id="contact">
       <ContactHeader>
         <ContactTitle>
-          <FormattedMessage
-            id="contact.questions"
-            defaultMessage="Questions?"
-          />
+          {intl.formatMessage({
+            id: 'contact.questions',
+            defaultMessage: 'Questions?',
+          })}
         </ContactTitle>
         <SocialNetworks>
-          <a target="_new" href="https://instagram.com/arrochaonoforro">
+          <SocialNetwork
+            target="_new"
+            href="https://instagram.com/arrochaonoforro"
+          >
             <SocialNetworkIcon className="fab fa-instagram" />
-          </a>
-          <a
+          </SocialNetwork>
+          <SocialNetwork
             target="_new"
             href="https://api.whatsapp.com/send?phone=5585999603638&text="
           >
             <SocialNetworkIcon className="fab fa-whatsapp" />
-          </a>
-          <a target="_new" href="mailto:arrochaonoforro@gmail.com">
+          </SocialNetwork>
+          <SocialNetwork target="_new" href="mailto:arrochaonoforro@gmail.com">
             <SocialNetworkIcon className="far fa-envelope" />
-          </a>
+          </SocialNetwork>
         </SocialNetworks>
       </ContactHeader>
       <MessageForm>
         <FormRow>
-          <InputForm type="text" placeholder="You Name" name="username" />
-          <InputForm type="email" placeholder="You Email" name="useremail" />
-          <InputForm type="phone" placeholder="You Email" name="userphone" />
+          <InputForm
+            type="text"
+            placeholder={intl.formatMessage({
+              id: 'contact.yourname',
+              defaultMessage: 'Your Name',
+            })}
+            name="username"
+          />
+          <InputForm
+            type="email"
+            placeholder={intl.formatMessage({
+              id: 'contact.youremail',
+              defaultMessage: 'Your Email',
+            })}
+            name="useremail"
+          />
+          <InputForm
+            type="phone"
+            placeholder={intl.formatMessage({
+              id: 'contact.yourphone',
+              defaultMessage: 'Your Phone',
+            })}
+            name="userphone"
+          />
         </FormRow>
         <FormRow>
           <MessageText name="usermessage" rows="20" cols="80"></MessageText>
         </FormRow>
-        <FormRow>
-          <button>
-            <i className="fas fa-paper-plane" />
+        <FormFooter>
+          <SendMessageButton>
+            <ButtonIcon className="fas fa-paper-plane" />
             Send Message
-          </button>
-        </FormRow>
+          </SendMessageButton>
+        </FormFooter>
       </MessageForm>
     </ContactContainer>
   );
-}
+};
+
+PageFooter.propTypes = {
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func,
+  }),
+};
+
+export default injectIntl(PageFooter);
--- src/components/contacts/Styles.jsx
+++ src/components/contacts/Styles.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { SECONDARY, TRANSPARENT, PRIMARY } from '../theme';
+import { SECONDARY, TRANSPARENT, PRIMARY, GRAY, WHITE } from '../theme';
 
 export const ContactContainer = styled.section`
   color: ${PRIMARY};
@@ -12,17 +12,31 @@ export const ContactContainer = styled.section`
 export const ContactHeader = styled.div`
   width: 970px;
   display: flex;
-  justify-content: space-around;
-  align-items: baseline;
-`;
+  justify-content: space-between;
+  align-items: center;
 
-export const SocialNetworks = styled.div``;
+  @media screen and (max-width: 980px) {
+    width: 768px;
+    flex-direction: column;
+    padding-bottom: 10px;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 650px;
+  }
+`;
 
 export const ContactTitle = styled.h2`
   font-size: 3.5em;
   font-weight: 700;
   line-height: 1.5em;
   text-transform: uppercase;
+`;
+
+export const SocialNetworks = styled.div`
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const SocialNetworkIcon = styled.i`
@@ -36,24 +50,90 @@ export const SocialNetworkIcon = styled.i`
   text-align: center;
 `;
 
+export const SocialNetwork = styled.a``;
+
 export const MessageForm = styled.form`
   width: 970px;
+
+  @media screen and (max-width: 980px) {
+    width: 768px;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 650px;
+  }
+
+  @media screen and (max-width: 655px) {
+    width: 95%;
+  }
 `;
 
 export const InputForm = styled.input`
   border: 3px solid ${PRIMARY};
   height: 2.35em;
+  width: 320px;
   font-size: 2em;
-  width: 100%;
+  padding-left: 5px;
+
+  @media screen and (max-width: 980px) {
+    margin-bottom: 5px;
+    width: 100%;
+  }
 `;
 
 export const MessageText = styled.textarea`
+  border: 3px solid ${PRIMARY};
   width: 100%;
   height: 200px;
   font-size: 2em;
+  padding-left: 5px;
 `;
 
 export const FormRow = styled.div`
   display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+
+  @media screen and (max-width: 980px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const FormFooter = styled.div`
+  display: flex;
   justify-content: center;
+  margin-bottom: 5px;
+
+  @media screen and (max-width: 980px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const SendMessageButton = styled.a`
+  background-color: ${GRAY};
+  color: ${WHITE};
+  text-align: center;
+  font-size: 2em;
+  padding: 5px;
+  word-wrap: none;
+  width: 350px;
+
+  &:visited,
+  &:active,
+  &:focus {
+    color: ${WHITE};
+    text-decoration: none;
+  }
+
+  &:hover {
+    background-color: ${PRIMARY};
+    color: ${WHITE};
+    text-decoration: none;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
 `;
